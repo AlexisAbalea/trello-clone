7b67c681099f939e1ba4245c140c2295c8af2e98
AlexisAb <abaleaalexis@gmail.com>
2022-01-02 18:08:59 +0100

    ajout composant colonne

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ColonneComponent } from './colonne/colonne.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ColonneComponent
   ],
   imports: [
     BrowserModule
